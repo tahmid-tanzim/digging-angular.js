5cefe83244021227eba5179cdf7163958031b57b
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-12-17 21:02:46 +0600

    Integrating Firebase Auth Registration

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,10 +1,22 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
+myApp.controller('RegistrationController',
+    ['$scope', '$firebaseAuth', 'FIREBASE_URL', function ($scope, $firebaseAuth, FIREBASE_URL) {
 
-    $scope.login = function () {
-        $scope.message = 'Welcome ' + $scope.user.email;
-    };
+        var ref  = new Firebase(FIREBASE_URL);
+        var auth = $firebaseAuth(ref);
 
-    $scope.register = function () {
-        $scope.message = 'Welcome ' + $scope.user.firstname;
-    };
-}]);
+        $scope.login = function () {
+            $scope.message = 'Welcome ' + $scope.user.email;
+        }; // login
+
+        $scope.register = function () {
+            auth.$createUser({
+                email: $scope.user.email,
+                password: $scope.user.password
+            }).then(function (newUser) {
+                console.log(newUser);
+                $scope.message = 'Welcome ' + $scope.user.firstname + '!, \nThanks for registering.';
+            }).catch(function (error) {
+                $scope.message = error.message;
+            }); // createUser
+        }; // register
+    }]); // Controller
