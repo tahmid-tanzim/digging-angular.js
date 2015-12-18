2e0a51f31192136f623d387681c0d2705277aa5e
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-12-18 21:01:20 +0600

    Creating Meetings in Firebase

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,22 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-    $scope.message = 'Yahoo!...';
-}]);
+myApp.controller('MeetingsController',
+    ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL', function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+    //$scope.message = 'Yahoo!...';
+        var ref = new Firebase(FIREBASE_URL);
+        var auth = $firebaseAuth(ref);
+
+        auth.$onAuth(function (authUser) {
+            if(authUser) {
+                var meetingsRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/meetings');
+                var meetingsInfo = $firebaseArray(meetingsRef);
+
+                $scope.addMeeting = function () {
+                    meetingsInfo.$add({
+                        name: $scope.meetingname,
+                        created_at: Firebase.ServerValue.TIMESTAMP
+                    }).then(function () {
+                        $scope.meetingname = '';
+                    }); // promise
+                } // AddMeetings
+            } // User Authenticated
+        }); // on Auth
+}]); // Controller
